fbb6fee9879d2a44681d798231669f7d382bec6a
LoserPanda <loserpanda82@gmail.com>
2018-08-28 09:44:08 +0300

    login page lol

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -5,7 +5,7 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <h1>Bananaproject</h1>
+                <h1>The Banana Project Login Page</h1>
             </div>
         );
     }
